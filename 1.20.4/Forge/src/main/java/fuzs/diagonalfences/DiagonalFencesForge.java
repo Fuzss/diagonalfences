94c5c3e3f093825bc83d272fe552490e7a014834
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-01 16:44:56 +0100

    prepare 1.20.4 port

---
+++ 1.20.4/Forge/src/main/java/fuzs/diagonalfences/DiagonalFencesForge.java
@@ -0,0 +1,16 @@
+package fuzs.diagonalfences;
+
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+
+@Mod(DiagonalFences.MOD_ID)
+@Mod.EventBusSubscriber(modid = DiagonalFences.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DiagonalFencesForge {
+
+    @SubscribeEvent
+    public static void onConstructMod(final FMLConstructModEvent evt) {
+        ModConstructor.construct(DiagonalFences.MOD_ID, DiagonalFences::new);
+    }
+}
