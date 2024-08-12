22385db204470bc4cf2a5815bbb26659fe7191d4
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-08-12 13:39:17 +0200

    full 1.21 port

--- 1.21/NeoForge/src/main/java/fuzs/diagonalfences/neoforge/client/DiagonalFencesNeoForgeClient.java
+++ 1.21/NeoForge/src/main/java/fuzs/diagonalfences/neoforge/client/DiagonalFencesNeoForgeClient.java
@@ -4,15 +4,12 @@ import fuzs.diagonalfences.DiagonalFences;
 import fuzs.diagonalfences.client.DiagonalFencesClient;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
 
-@Mod.EventBusSubscriber(modid = DiagonalFences.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod(value = DiagonalFences.MOD_ID, dist = Dist.CLIENT)
 public class DiagonalFencesNeoForgeClient {
 
-    @SubscribeEvent
-    public static void onConstructMod(final FMLConstructModEvent evt) {
+    public DiagonalFencesNeoForgeClient() {
         ClientModConstructor.construct(DiagonalFences.MOD_ID, DiagonalFencesClient::new);
     }
 }
